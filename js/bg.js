777652b7e04208aa3b53d36195fd58abe13c4f1d
ggc1412 <ggc1412@gmail.com>
2020-02-15 21:39:40 +0900

    merge task

--- js/bg.js
+++ js/bg.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const body = document.querySelector('body');
 const IMG_NUMBER = 5;
 
@@ -19,25 +18,3 @@ function init() {
   paintImage(randomNumber);
 }
 init();
-=======
-const body = document.querySelector('body');
-const IMG_NUMBER = 5;
-
-function paintImage(imgNumber) {
-  const image = new Image();
-  image.src = `images/${imgNumber + 1}.jpg`;
-  image.classList.add('bgImage');
-  body.appendChild(image);
-}
-
-function genRandom(){
-  const number = Math.floor(Math.random() * IMG_NUMBER);
-  return number;
-}
-
-function init() {
-  const randomNumber = genRandom();
-  paintImage(randomNumber);
-}
-init();
->>>>>>> 01e18bc804fee0fd474e14ef615514a55500eef4
--- js/greeting.js
+++ js/greeting.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const nameDiv = document.querySelector('.js-name'),
       nameForm = nameDiv.querySelector('form'),
       nameSpan = nameDiv.querySelector('span');
@@ -35,41 +34,3 @@ function init() {
   nameForm.addEventListener('submit', saveName);
 }
 init();
-=======
-const nameDiv = document.querySelector('.js-name'),
-      nameForm = nameDiv.querySelector('form'),
-      nameSpan = nameDiv.querySelector('span');
-const nameInput = nameForm.querySelector('input');
-const toDoDiv = document.querySelector('.js-to-do-div');
-
-
-function saveName(e) {
-  e.preventDefault();
-  const name = nameInput.value;
-  localStorage.setItem('name', name);
-  paintGreeting(name);
-}
-
-function paintGreeting(name) {
-  nameForm.classList.add('none');
-  nameSpan.classList.remove('none');
-  toDoDiv.classList.remove('none');
-  nameSpan.innerText = `Hello ${name}!`;
-}
-
-function checkName(){
-  const name = localStorage.getItem('name');
-  if(name){
-    paintGreeting(name);
-  }else {
-    toDoDiv.classList.add('none');
-    nameSpan.classList.add('none');
-  }
-}
-
-function init() {
-  checkName();
-  nameForm.addEventListener('submit', saveName);
-}
-init();
->>>>>>> 01e18bc804fee0fd474e14ef615514a55500eef4
